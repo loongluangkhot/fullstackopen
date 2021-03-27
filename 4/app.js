6dc04e48f8811d3e811b6b81a3a800d4f86b462d
loongluangkhot <loongluangkhot@gmail.com>
2021-03-27 20:51:16 +0800

    WIP 4.16

--- 4/app.js
+++ 4/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
+require("express-async-errors");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const config = require("./utils/config");
+const userRouter = require("./controllers/users");
 const blogsRouter = require("./controllers/blogs");
+const logger = require("./utils/logger");
 
 mongoose.connect(config.MONGODB_URI, {
   useNewUrlParser: true,
@@ -14,6 +17,17 @@ mongoose.connect(config.MONGODB_URI, {
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/users", userRouter);
 app.use("/api/blogs", blogsRouter);
+app.use("", (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+});
+app.use("", (error, request, response, next) => {
+  if (error.name === "ValidationError") {
+    response.status(400).json({ error: error.message });
+  }
+  logger.error(error.message);
+  next(error);
+});
 
 module.exports = app;
--- 4/controllers/blogs.js
+++ 4/controllers/blogs.js
@@ -7,31 +7,19 @@ router.get("", async (request, response) => {
 });
 
 router.post("", async (request, response) => {
-  try {
-    const blog = new Blog(request.body);
-    const result = await blog.save();
-    return response.status(201).json(result);
-  } catch (e) {
-    return response.status(400).json(e);
-  }
+  const blog = new Blog(request.body);
+  const result = await blog.save();
+  response.status(201).json(result);
 });
 
 router.delete("/:id", async (request, response) => {
-  try {
-    await Blog.findByIdAndDelete(request.params.id);
-    response.status(204).end();
-  } catch (e) {
-    return response.status(400).json(e);
-  }
+  await Blog.findByIdAndDelete(request.params.id);
+  response.status(204);
 });
 
 router.put("/:id", async (request, response) => {
-  try {
-    const blog = await Blog.findByIdAndUpdate(request.params.id, request.body, {new: true});
-    response.status(200).json(blog);
-  } catch (e) {
-    return response.status(400).json(e);
-  }
+  const blog = await Blog.findByIdAndUpdate(request.params.id, request.body, {new: true});
+  response.status(200).json(blog);
 })
 
 module.exports = router;
